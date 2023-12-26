0563d5c9b0acf9f9d343560624b775d7a1a3ef52
firefishmoon <hj0467@gmail.com>
2023-12-26 18:07:16 +0800

    quote charactor

--- pages/about.tsx
+++ pages/about.tsx
@@ -8,7 +8,7 @@ const About = () => (
         <Container bgColor='gray.900'>
             <Image fit="cover" src="https://www.notion.so/images/page-cover/nasa_robert_stewart_spacewalk_2.jpg"
                 w="100%" h='300'></Image>
-            <Text>Hi, I'm a software developer based in Chengdu China.</Text>
+            <Text>Hi, I&apos;m a software developer based in Chengdu China.</Text>
             <Text>Things I like:</Text>
             <Text>VideoGame, GameDev, AI, Math, Drawing</Text>
         </Container>
